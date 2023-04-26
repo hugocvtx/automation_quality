d694d46818a79ffc2c54652a5911a5d2b2254baa
hugocvtx <hugo.cvtx@gmail.com>
2023-04-26 17:48:47 -0300

    Exercício 2 - Automacao

---
+++ cypress/e2e/examples/features/Login.feature
@@ -0,0 +1,7 @@
+Feature: Login automação 
+
+    Scenario: Colocar login errado no site
+    Given Eu acesso a home page
+    When Acesso a página de login
+    And Digito dados inválidos
+    Then Deve aparecer mensagem de erro
--- cypress/support/step_definitions/steps.js
+++ cypress/support/step_definitions/steps.js
@@ -8,3 +8,23 @@ When("I search for Grupo Quality", () => {
     cy.get('[title="Pesquisar"]').type('Quality Digital{enter}')
 })
 
+
+// Cenário Automation exercise 2
+
+Given("Eu acesso a home page", () => {
+    cy.visit("https://automationexercise.com/");
+})
+
+When("Acesso a página de login", () => {
+    cy.get('.shop-menu > .nav > :nth-child(4)').click();
+})
+
+And("Digito dados inválidos", () => {
+    cy.get('[data-qa="login-email"]').type("teste@teste.com.br")
+    cy.get('[data-qa="login-password"]').type("123456")
+    cy.get('[data-qa="login-button"]').click()
+})
+
+Then("Deve aparecer mensagem de erro", () => {
+	cy.get('.login-form > form > p').should('have.text', "Your email or password is incorrect!");
+})
